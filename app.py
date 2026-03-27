83a7dea7768116f4a2f2550ae11ccf5bd58d9200
Dhanush Rajashekar <dhanushrajashekar1315gmail.com>
2026-03-27 18:10:33 +0000

    New File upload button has been added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from transactions import flag_transaction,df
 
 
@@ -9,10 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 def welcome_Page():
     return render_template('index.html')
+
+#Uploading files here 
+@app.route('/uploads', methods=['POST'])
+def file_upload():
+        
+    if 'files' not in request.files:
+        return "File not present"
+    present_file = request.files['files']
+    save_Path = ('uploads/' + present_file.filename)
+    present_file.save(save_Path)
+    return 'File Uploaded Successfully'
     
 
 df['risk_flag'] = df.apply(flag_transaction, axis=1)
 
+#Report Values from transaction.py
 @app.route('/report')
 def final_report():
     counts = df['risk_flag'].value_counts()
@@ -21,8 +33,8 @@ def final_report():
     clean = counts['Clean']
     return render_template('report.html', fraud=fraud, sus=sus, clean=clean)
     
-    
-   
 
+ 
+   
 if __name__ == "__main__":
     app.run(debug=True)
