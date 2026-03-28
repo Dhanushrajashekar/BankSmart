2415f0c21e738abe16f04b3fd4d99e4b1bf99dc7
Dhanush Rajashekar <dhanushrajashekar1315gmail.com>
2026-03-28 14:41:55 +0000

    Added SQLite Database - saves all the analysis automatically

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask,render_template,request
-from transactions import flag_transaction,df
-
-
+from transactions import flag_transaction
+import pandas as pd
 import os
+import sqlite3
+from datetime import date as dt
+
+
+def save_to_db(filename, date, fraud, suspicious, clean):
+    conn = sqlite3.connect('database.db')    # Create a cursor object to execute SQL statements
+    cur = conn.cursor()
+    cur.execute('''INSERT into BankSmart(fileName, date, fraud, suspicious, clean) Values(?,?,?,?,?)''',(filename,str(dt.today()), fraud, suspicious, clean))
+    conn.commit()
+    conn.close()
+
+    
+
+
 print("RUNNING FROM:", os.path.abspath(__file__))
 app = Flask(__name__)
+
 
 @app.route('/')
 def welcome_Page():
@@ -19,18 +33,16 @@ def file_upload():
     present_file = request.files['files']
     save_Path = ('uploads/' + present_file.filename)
     present_file.save(save_Path)
-    return 'File Uploaded Successfully'
-    
 
-df['risk_flag'] = df.apply(flag_transaction, axis=1)
+    new_df = pd.read_csv(save_Path)
+    new_df['risk_flag'] = new_df.apply(flag_transaction, axis=1)
 
-#Report Values from transaction.py
-@app.route('/report')
-def final_report():
-    counts = df['risk_flag'].value_counts()
+    today = dt.today()
+    counts = new_df['risk_flag'].value_counts()
     fraud = counts['Fraud']
     sus = counts['Suspecious']
     clean = counts['Clean']
+    save_to_db(present_file.filename, str(today) , int(fraud), int(sus), int(clean))
     return render_template('report.html', fraud=fraud, sus=sus, clean=clean)
     
 
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 ================================
 BANKSMART FRAUD REPORT
-Generated: 2026-03-27
+Generated: 2026-03-28
 ================================
 Total accounts analysed: 3,000
 Total amount at risk: 59,647,615
